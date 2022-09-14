72114185401a075f07f02c3fcecefc16be7f84d5
Hari Kishore <88875466+harikishore8@users.noreply.github.com>
2022-09-15 00:00:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,29 +4,51 @@ import Person from "./Components/Person";
 
 class App extends Component {
   state = {
-    //is state is changed then react re-renders the dom to check for changes
+    //if state is changed then react re-renders the dom to check for changes
     people: [
       { name: "hari", age: 19 },
       { name: "romu", age: 20 },
-      { nmae: "pooja", age: 23 },
+      { name: "pooja", age: 23 },
     ],
   };
 
-  switchbuttonhandler = () => {
-    console.log(this);
-    alert(this.state.people[0].name);
+  switchbutthandler = (abc) => {
+    // this.state.people[0].name = 'abc'; --> this wont work as we cannot directly modify the state use setState method instead!
+    this.setState({
+      people: [
+        { name: abc, age: 19 },
+        { name: "romu", age: 20 },
+        { name: "pooja", age: 23 },
+      ],
+    });
+  };
+
+  nameChangeHandler = (event) => {
+    // this.state.people[0].name = 'abc'; --> this wont work as we cannot directly modify the state use setState method instead!
+    this.setState({
+      people: [
+        { name: "hari", age: 19 },
+        { name: event.target.value, age: 20 },
+        { name: "pooja", age: 23 },
+      ],
+    });
   };
 
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
-        <button onClick={this.switchbuttonhandler}>Switch name</button>
+        <button onClick={this.switchbutthandler}>Switch name</button>
         <Person
           name={this.state.people[0].name}
           age={this.state.people[0].age}
         />
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}>
+        <Person
+          name={this.state.people[1].name}
+          age={this.state.people[1].age}
+          // click={this.switchbutthandler.bind(this, "happy")}
+          change={this.nameChangeHandler}
+        >
           Hobbies: f1
         </Person>
         <Person
