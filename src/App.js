9785d5a086bbb5159c1fc3c9401b4e9742e67de9
Hari Kishore <88875466+harikishore8@users.noreply.github.com>
2022-09-22 23:16:08 +0530

    list rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import person from "./Components/Person";
 import Person from "./Components/Person";
 
 class App extends Component {
   state = {
     //if state is changed then react re-renders the dom to check for changes
     people: [
-      { name: "hari", age: 19 },
-      { name: "romu", age: 20 },
-      { name: "pooja", age: 23 },
+      { id: "1", name: "hari", age: 19 },
+      { id: "2", name: "romu", age: 20 },
+      { id: "3", name: "pooja", age: 23 },
     ],
     showpersons: true,
   };
@@ -24,20 +25,16 @@ class App extends Component {
     });
   };
 
-  nameChangeHandler = (event) => {
-    // this.state.people[0].name = 'abc'; --> this wont work as we cannot directly modify the state use setState method instead!
-    this.setState({
-      people: [
-        { name: "hari", age: 19 },
-        { name: event.target.value, age: 20 },
-        { name: "pooja", age: 23 },
-      ],
-    });
-  };
-
   showpersonhandler = () => {
     const per = this.state.showpersons;
     this.setState({ showpersons: !per });
+  };
+
+  deleteNameHandler = (peopleIndex) => {
+    // const people = this.state.people; this is not a good practice as we are directly changing the original state(mutate same state) inorder to avoid to that we have to copy the contents from the state first and then change it afterwards using spread operator
+    const people = [...this.state.people];
+    people.splice(peopleIndex, 1);
+    this.setState({ people });
   };
 
   render() {
@@ -45,22 +42,16 @@ class App extends Component {
     if (this.state.showpersons) {
       rend = (
         <div>
-          <Person
-            name={this.state.people[0].name}
-            age={this.state.people[0].age}
-          />
-          <Person
-            name={this.state.people[1].name}
-            age={this.state.people[1].age}
-            // click={this.switchbutthandler.bind(this, "happy")}
-            change={this.nameChangeHandler}
-          >
-            Hobbies: f1
-          </Person>
-          <Person
-            name={this.state.people[2].name}
-            age={this.state.people[2].age}
-          />
+          {this.state.people.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deleteNameHandler(index)}
+                key={person.id}
+              />
+            );
+          })}
         </div>
       );
     }
